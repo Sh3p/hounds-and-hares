3e925eaf7ade61843214f3eeba3dfc8bf26584f0
sam <sjnoggle@millersville.edu>
2023-02-27 16:25:36 -0500

    turning the class modular

--- harev2.py
+++ harev2.py
@@ -12,11 +12,39 @@ class HoundsAndHare:
         self.board = ["_"] * 9
         self.board[0] = "Hare"
         self.turn = "H"
-    
+
+    def reset(self):
+        """
+        Resets the starting board state.
+        """
+        pass
+
+    def __str__(self):
+        pass
+
+    def boardToStr(self, board):
+        """
+        Returns a string representation of the konane board.
+        """
+        pass
+
+    def valid(self, row, col):
+        """
+        Returns true if the given row and col represent a valid location on
+        the h&h board.
+        """
+        pass
+
     def switch_turn(self):
         self.turn = "H" if self.turn == "Hare" else "Hare"
     #determines if game ends
+
     def is_game_over(self):
+        """
+        Returns true if the hare has reached the 
+        leftmost position, or has passed the leftmost hound
+
+        """
         if "_" not in self.board:
             return True
         
@@ -29,54 +57,57 @@ class HoundsAndHare:
                 return True
         
         return False
-    #algorithm used to determine moves
-    def minimax(self, depth, is_maximizing):
-        if depth == 0 or self.is_game_over():
-            return 0
+
+
+    # Code for the AI player 
+
+    # def minimax(self, depth, is_maximizing):
+    #     if depth == 0 or self.is_game_over():
+    #         return 0
         
-        if is_maximizing:
-            max_value = -1
-            for i, cell in enumerate(self.board):
-                if cell == "_":
-                    self.board[i] = self.turn
-                    value = self.minimax(depth - 1, False)
-                    self.board[i] = "_"
-                    max_value = max(max_value, value)
-            return max_value
-        else:
-            min_value = 1
-            for i, cell in enumerate(self.board):
-                if cell == "_":
-                    self.board[i] = self.turn
-                    value = self.minimax(depth - 1, True)
-                    self.board[i] = "_"
-                    min_value = min(min_value, value)
-            return min_value
-    #determines the best move of possible moves from minimax
-    def best_move(self):
-        max_value = -1
-        best_move = None
-        for i, cell in enumerate(self.board):
-            if cell == "_":
-                self.board[i] = self.turn
-                value = self.minimax(9, False)
-                self.board[i] = "_"
-                if value > max_value:
-                    max_value = value
-                    best_move = i
-        return best_move
+    #     if is_maximizing:
+    #         max_value = -1
+    #         for i, cell in enumerate(self.board):
+    #             if cell == "_":
+    #                 self.board[i] = self.turn
+    #                 value = self.minimax(depth - 1, False)
+    #                 self.board[i] = "_"
+    #                 max_value = max(max_value, value)
+    #         return max_value
+    #     else:
+    #         min_value = 1
+    #         for i, cell in enumerate(self.board):
+    #             if cell == "_":
+    #                 self.board[i] = self.turn
+    #                 value = self.minimax(depth - 1, True)
+    #                 self.board[i] = "_"
+    #                 min_value = min(min_value, value)
+    #         return min_value
+    # #determines the best move of possible moves from minimax
+    # def best_move(self):
+    #     max_value = -1
+    #     best_move = None
+    #     for i, cell in enumerate(self.board):
+    #         if cell == "_":
+    #             self.board[i] = self.turn
+    #             value = self.minimax(9, False)
+    #             self.board[i] = "_"
+    #             if value > max_value:
+    #                 max_value = value
+    #                 best_move = i
+    #     return best_move
 
 
-#exceutes a game
-def play_game():
-    game = HoundsAndHare()
-    while not game.is_game_over():
-        print("Current Board:", " ".join(game.board))
-        move = game.best_move()
-        print("Best move:", move)
-        game.board[move] = game.turn
-        game.switch_turn()
-        print("Final Board:", " ".join(game.board))
+# #exceutes a game
+# def play_game():
+#     game = HoundsAndHare()
+#     while not game.is_game_over():
+#         print("Current Board:", " ".join(game.board))
+#         move = game.best_move()
+#         print("Best move:", move)
+#         game.board[move] = game.turn
+#         game.switch_turn()
+#         print("Final Board:", " ".join(game.board))
 
 
-play_game()
+# play_game()
